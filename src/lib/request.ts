a2cb3560c68ab1aa6cd51f96173e5f3cdd0e9f75
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2022-12-23 21:43:51 +0100

    refactor: pass svelte checks

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,14 +1,14 @@
-export default function request<V, T>(options: RequestOptions<V, T> & { fetch: typeof fetch }) {
+import { GraphQLClient, type RequestOptions, type Variables } from 'graphql-request';
+
+import { PUBLIC_ANILIST_GQL_SERVER_URL } from '$env/static/public';
+
+export default function request<V extends Variables, T>(options: RequestOptions<V, T> & { fetch: typeof fetch }) {
 	const client = new GraphQLClient(PUBLIC_ANILIST_GQL_SERVER_URL, {
 		fetch: options.fetch,
-		errorPolicy: 'Ignore'
-	});
+		errorPolicy: 'ignore'
+	});	
 
 
 	return client.request<T, V>(options);
-}
-
-import { GraphQLClient, type RequestOptions } from 'graphql-request';
-
-import { PUBLIC_ANILIST_GQL_SERVER_URL } from '$env/static/public';
+}	
 
